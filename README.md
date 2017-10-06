c30ed66e074514dd6bd97b6cd5a91b771f70c05e
Deep Mistry <dmistry@cars.com>
2017-10-06 13:59:10 -0500

    readme updates | cleanup

--- README.md
+++ README.md
@@ -9,3 +9,20 @@ Updates an existing docker swarm secret
 
 *Usage*
 
+```bash
+$ swarm-tool rotate-secrets --secret secret --secret-file=/path/to/updated-secret-data
+```
+`rotate-secrets` will do the following -
+
+* Check if the `secret` exists
+* If there are services which are using this secret...
+    * Creates a new `temp_secret` with data from `secret-file`
+    * Updates services by removing `secret` and adding `temp_secret`
+    * Wait for service updates to converge
+    * Updates the `secret` with data from `secret-file`
+    * Updates services again. This time removing the `temp_secret` and adding the updated `secret`
+    * Wait for service updates to converge
+    * Removes the `temp_secret`
+* If there are no services which are using this secret...
+    * Removes the `secret`
+    * Create `secret` with data from the `secret-file`
--- cmd/rotate_secret.go
+++ cmd/rotate_secret.go
@@ -1,17 +1,3 @@
-// Copyright © 2017 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 package cmd
 
 import (
@@ -29,15 +15,24 @@ var (
 // rotateSecretCmd represents the rotateSecret command
 var rotateSecretCmd = &cobra.Command{
 	Use:   "rotate-secret",
-	Short: "Rotate a secret for a docker service",
-	Long: `Rotate a secret for a docker services.
+	Short: "Rotate a secret in a Docker Swarm cluster",
+	Long: `Rotate a secret in a Docker Swarm cluster.
 
-rotate-secret will do the following
+rotate-secret will do the following -
 
-1) Create a new temp secret with a provided file or string
-2) Update a/all sevice/services by adding temp secret and removing the old one
-3) Remove the old secret
-4) Update the temp secret name with old secret name`,
+
+* Check if the secret exists
+* If there are services which are using this secret...
+    * Creates a new temp_secret with data from secret-file
+    * Updates services by removing secret and adding temp_secret
+    * Wait for service updates to converge
+    * Updates the secret with data from secret-file
+    * Updates services again. This time removing the temp_secret and adding the updated secret
+    * Wait for service updates to converge
+    * Removes the temp_secret
+* If there are no services which are using this secret...
+    * Removes the secret
+    * Create secret with data from the secret-file`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		if len(secret) == 0 {
 			return errors.New("secret is required")
@@ -57,6 +52,5 @@ rotate-secret will do the following
 func init() {
 	RootCmd.AddCommand(rotateSecretCmd)
 	rotateSecretCmd.Flags().StringVarP(&secret, "secret", "s", "", "Secret to rotate")
-	rotateSecretCmd.Flags().StringVarP(&secretFile, "secret-file", "f", "", "Secret file to update the secret with")
-
+	rotateSecretCmd.Flags().StringVarP(&secretFile, "secret-file", "f", "", "File with new secret data")
 }
